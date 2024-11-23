c5f85a3ef1bf4d1ca143f9d1632c195ec8317c69
= <=>
2024-11-23 11:06:05 +0530

    gonna work with branhces here on then

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from './Comps/Nav';
 import Home from './Comps/Home';
 import Login from './Comps/Login';
 import Donation_Form from './Comps/Donation-Form';
+import Login1 from './Comps/Login1';
+import SignupPage from './Comps/signup';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SignupPage1 from './Comps/SIGNUP1';
+
 function App() {
   
   return (
@@ -14,9 +19,18 @@ function App() {
       <Login>
 
       </Login>
+      <SignupPage>  </SignupPage>
       <Donation_Form></Donation_Form>
-      
+
+      <SignupPage1></SignupPage1>
+        
+        
+        
+
+<Login1></Login1>      
     </div>
+
+
   );
 }
 
